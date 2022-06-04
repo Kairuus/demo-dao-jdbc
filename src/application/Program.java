c4c1c798be204a05ecb22769dcd99b49e2167749
Victor Muniz <kingkairuus@gmail.com>
2022-06-04 16:41:08 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1,"Books");
-		System.out.println(obj);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Seller seller = new Seller(1,"Kairuus","kingkairuus@gmail.com",new Date(),2000.0,new Department(1,"Books"));
+		
+		System.out.println(seller);
 	}
 
 }
