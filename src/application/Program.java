1fc89297cf045046757d6d2b728343801ceabd8c
Victor Muniz <kingkairuus@gmail.com>
2022-06-06 12:12:57 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,10 +10,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Seller seller = new Seller(1,"Kairuus","kingkairuus@gmail.com",new Date(),2000.0,new Department(1,"Books"));
-		
-		System.out.println(seller);
+		SellerDao sellerDao
 	}
 
 }
